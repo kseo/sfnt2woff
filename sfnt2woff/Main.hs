3c1ffadc387a3f1aeb5f75c9e19cd81981487bd5
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-07 21:59:27 +0900

    Add command line options to specify metadata file and private data file

--- sfnt2woff/Main.hs
+++ sfnt2woff/Main.hs
@@ -3,20 +3,32 @@ module Main where
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Word
+import Data.IORef
 import Graphics.WOFF
 import Options.Applicative
 import System.Exit
 import System.FilePath
 
 data CommandOption = CommandOption
-  { otffile :: String
+  { metadataFile :: Maybe String
+  , privateDataFile :: Maybe String
+  , otfFile :: String
   } deriving (Show)
 
 opts = info (helper <*> cmdOpt)
   ( fullDesc
   <> progDesc "package OpenType <otffile> as WOFF, creating <otffile>.woff")
   where
-    cmdOpt = CommandOption <$> argument str (metavar "otffile")
+    cmdOpt = CommandOption
+      <$> optional (strOption
+          ( short 'm'
+          <> metavar "metadata.xml"
+          <> help "include metadata from <metadata.xml> (not validated)"))
+      <*> optional (strOption
+          ( short 'p'
+          <> metavar "private.data"
+          <> help "include private data block"))
+      <*> argument str (metavar "otffile")
 
 majorVersion :: Word16
 majorVersion = 0
@@ -24,17 +36,41 @@ majorVersion = 0
 minorVersion :: Word16
 minorVersion = 0
 
+whenJust :: Applicative f => Maybe a -> (a -> f ()) -> f ()
+whenJust (Just x) s = s x
+whenJust Nothing _ = pure ()
+
 main :: IO ()
 main = do
+  woffRef <- newIORef (BS.empty)
   cmd <- execParser opts
-  let infile = otffile cmd
+  let infile = otfFile cmd
   sfnt <- BS.readFile infile
   result <- encode sfnt majorVersion minorVersion
-  case result of
-    Left errorCode -> do
-      print errorCode
-      exitWith (ExitFailure 1)
-    Right (woff, warnings) -> do
-      let outfile = replaceExtension infile ".woff"
-      BS.writeFile outfile woff
+  checkResult result woffRef
+
+  whenJust (metadataFile cmd) (\f -> do
+    meta <- BS.readFile f
+    woff <- readIORef woffRef
+    result <- setMetadata woff meta
+    checkResult result woffRef)
+
+  whenJust (privateDataFile cmd) (\f -> do
+    priv <- BS.readFile f
+    woff <- readIORef woffRef
+    result <- setPrivateData woff priv
+    checkResult result woffRef)
+
+  let outfile = replaceExtension infile ".woff"
+  woff <- readIORef woffRef
+  BS.writeFile outfile woff
+
+  where
+    checkResult result woffRef =
+      case result of
+        Left errorCode -> do
+          print errorCode
+          exitWith (ExitFailure 1)
+        Right (woff, warnings) -> do
+          writeIORef woffRef woff
 
